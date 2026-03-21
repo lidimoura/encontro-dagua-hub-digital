95cb0fd49e28adfed306c8e16e81dbb73d58ee82
lidimoura <lidimfc@gmail.com>
2026-03-21 01:04:26 -0400

    fix: remove duplicate addActivity/deleteDeal from useCRM destructuring

--- src/features/boards/components/Modals/DealDetailModal.tsx
+++ src/features/boards/components/Modals/DealDetailModal.tsx
@@ -97,7 +97,6 @@ export const DealDetailModal: React.FC<DealDetailModalProps> = ({ dealId, isOpen
     moveDeal,
     updateDeal,
     deleteDeal,
-    addActivity,
     activities,
     addActivity,
     updateActivity,
